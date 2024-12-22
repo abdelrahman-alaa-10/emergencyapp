1298be973519867e5ed2b5427637164e582b810b
abdelrahman-alaa-10 <abdelrahmanalaa846@gmail.com>
2024-12-22 11:07:09 +0200

    Channel Update

--- lib/utils/videocallsettings.dart
+++ lib/utils/videocallsettings.dart
@@ -4,6 +4,6 @@ class Settings
 
   static const String appID = "7945a7b7b6e14ccd8b7a705c68a39bdf";
   static const String appCertificate = "4dc59a357fa04589a9f4a0968e481654";
-  static const String channelName = "EmergencyVideoCall";
-  static const String token = "007eJxTYLDmOFnQItvpd0Roho/AqmPlJQIHqqcviZywauP01OyfGs8UGMwtTUwTzZPMk8xSDU2Sk1MskswTzQ1Mk80sEo0tk1LSKr3S0xsCGRkeaF9jYmSAQBCfnyEsMyU13zkxJyfeyEjf0IiBAQCd9yKz";  
+  static const String channelName = "testCall";
+  static const String token = "007eJxTYLi9yzgufHNv44F/3xYeiiqd0pR5xZ2t2Lp2klhr+Kvyh7sVGMwtTUwTzZPMk8xSDU2Sk1MskswTzQ1Mk80sEo0tk1LS+q+lpzcEMjLIv57AwAiFID4HQ0lqcYlzYk4OAwMAk8kjmA==";  
 }
